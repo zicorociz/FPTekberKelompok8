379af332738df2a28e0590a8011a87c717e39c5b
zicorociz <khaliszikrul16@gmail.com>
2025-06-01 06:30:56 +0700

    update layanan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'login.dart'; // Import login.dart di sini
+//import 'login.dart'; // Import login.dart di sini
+import 'layanan.dart'; // Import layanan.dart di sini
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginPage(), // Set LoginPage sebagai home page
+      home: LayananPage(), // Set LayananPage sebagai home page
     );
   }
 }
